e82bd178c2a52911087180b6b85e82b5692ddbc4
B.N. Berrevoets <bert@berrevoets.net>
2024-12-08 10:36:05 +0100

    Adding the Users REST API
    Add user management controllers and DTOs
    
    - Added `HomeController` with `Index` action in `HomeController.cs`.
    - Introduced `UsersController` with actions for user CRUD operations.
    - Created `UserDto` and `UpdateUserDto` in `Dtos.cs`.
    - Added extension method to convert `ApplicationUser` to `UserDto`.
    - Updated `_CookieConsentPartial.cshtml` with model declaration.

---
+++ Play.Identity.Service/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Play.Identity.Service.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Play.Identity.Service/Controllers/UsersController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Play.Identity.Service.Dtos;
+using Play.Identity.Service.Entities;
+
+namespace Play.Identity.Service.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public UsersController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<UserDto>> Get()
+    {
+        var users = _userManager.Users
+            .ToList()
+            .Select(user => user.AsDto());
+
+        return Ok(users);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<UserDto>> GetByIdAsync(Guid id)
+    {
+        var user = await _userManager.FindByIdAsync(id.ToString());
+
+        if (user == null) return NotFound();
+
+        return user.AsDto();
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> PutAsync(Guid id, UpdateUserDto userDto)
+    {
+        var user = await _userManager.FindByIdAsync(id.ToString());
+
+        if (user == null) return NotFound();
+
+        user.Email = userDto.Email;
+        user.UserName = userDto.Email;
+        user.Gil = userDto.Gil;
+
+        await _userManager.UpdateAsync(user);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteAsync(Guid id)
+    {
+        var user = await _userManager.FindByIdAsync(id.ToString());
+
+        if (user == null) return NotFound();
+
+        await _userManager.DeleteAsync(user);
+
+        return NoContent();
+    }
+}
